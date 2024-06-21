4aa4ec80620bd21767899e5bfa5fbe7449a1487d
Pavan <117716956+Nagi2003@users.noreply.github.com>
2024-06-21 14:25:48 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'Nagendra',
+    author_email= 'maddela3435@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
